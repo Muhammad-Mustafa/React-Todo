e2f9000e2a2ca3ffe499b32abda7dff95e424503
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-07-20 18:41:45 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,11 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-      count: 0,
-    }
-  };
-
-  increaseCount(){
-    this.setState({count: this.state.count + 1});
-    this.setState({count: this.state.count + 2});
-    this.setState({count: this.state.count + 1});
-
-  }
-
   render(){
     return(
-      <>
-      <h1>Hello world</h1>
       <div>
-        {/* <button onClick={this.increaseCount.bind(this)}>Increase</button> */}
-        <button onClick={() =>  this.increaseCount()}>Increase</button>
+        Hello World 
       </div>
-      <h3>{this.state.count}</h3>
-      </>
     )
   }
 }
